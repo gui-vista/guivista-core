82335ad5077659e9a053d2c775438a1a5abf95c3
Nick Apperley <napperley@gmail.com>
2020-01-21 15:22:52 +1300

    Provided a def file for each target.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,20 +14,25 @@ repositories {
 kotlin {
     linuxX64("linuxX64") {
         compilations.getByName("main") {
-            cinterops.create("glib2") {
+            cinterops.create("glib2_x64") {
                 includeDirs("/usr/include/glib-2.0")
             }
         }
-        linuxArm32Hfp("linuxArm32")
-
-        sourceSets {
-            @Suppress("UNUSED_VARIABLE") val linuxX64Main by getting {
-                languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
+    }
+    linuxArm32Hfp("linuxArm32") {
+        compilations.getByName("main") {
+            cinterops.create("glib2_arm32") {
+                includeDirs("/mnt/pi_image/usr/include/glib-2.0")
             }
-            @Suppress("UNUSED_VARIABLE") val linuxArm32Main by getting {
-                dependsOn(linuxX64Main)
-                languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
-            }
+        }
+    }
+    sourceSets {
+        @Suppress("UNUSED_VARIABLE") val linuxX64Main by getting {
+            languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
+        }
+        @Suppress("UNUSED_VARIABLE") val linuxArm32Main by getting {
+            dependsOn(linuxX64Main)
+            languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
         }
     }
 }
