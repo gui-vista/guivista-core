f2a194b1f15d397fe090521e64ef7972ccda2eee
Nick Apperley <napperley@gmail.com>
2021-01-08 15:29:15 +1300

    Improved how errors are handled with the Error class.

--- src/commonMain/kotlin/org/guiVista/core/Error.kt
+++ src/commonMain/kotlin/org/guiVista/core/Error.kt
@@ -1,4 +1,45 @@
 package org.guiVista.core
 
 /** A system for reporting a error. */
-public expect class Error : Closable
+public expect class Error : Closable {
+    public companion object {
+        /**
+         * Creates a new [Error] instance. Unlike `g_error_new()`, [message] isn't a `printf` style format string. Use
+         * this function if [message] contains text you don't have control over, that could include `printf` escape
+         * sequences.
+         * @param domain Error domain.
+         * @param code Error code.
+         * @param message Error message.
+         * @return A new [Error] instance.
+         */
+        public fun fromLiteral(domain: UInt, code: Int, message: String): Error
+
+        /**
+         * If [dest] is *null* then free [src] otherwise move [src] into [dest]. The error variable [dest] points to
+         * **MUST** be *null*. Note that [src] is no longer valid after this call. If you want to keep using the same
+         * [Error], you need to set it to *null* after calling this function on it.
+         */
+        public fun propagate(dest: Error?, src: Error)
+    }
+
+    /**
+     * Alters the [error's][Error] literal. Use this function if [message] contains text you don't have control
+     * over. That could include `printf()` escape sequences.
+     * @param domain The error domain.
+     * @param code The error code.
+     * @param message The error message.
+     */
+    public fun changeLiteral(domain: UInt, code: Int, message: String)
+
+    /**
+     * Checks to see if the error matches the [domain], and [code]. In particular when error is *null*, *false* will be
+     * returned. If [domain] contains a FAILED (or otherwise generic) error code you should generally not check for it
+     * explicitly, but should instead treat any not explicitly recognized error code as being equivalent to the FAILED
+     * code. This way if the [domain] is extended in the future to provide a more specific error code for a certain
+     * case, your code will still work.
+     * @param domain An error domain.
+     * @param code An error code.
+     * @return If error matches [domain], and [code] then *true*, otherwise *false*.
+     */
+    public fun matches(domain: UInt, code: Int): Boolean
+}
--- src/linuxX64Main/kotlin/org/guiVista/core/Error.kt
+++ src/linuxX64Main/kotlin/org/guiVista/core/Error.kt
@@ -6,48 +6,26 @@ import kotlinx.cinterop.*
 public actual class Error private constructor(errorPtr: CPointer<GError>?) : Closable {
     public val gErrorPtr: CPointer<GError>? = errorPtr
 
-    public companion object {
+    public actual companion object {
         /** Creates a new [Error] instance from a errorPtr. */
-        public fun fromErrorPtr(errorPtr: CPointer<GError>): Error = Error(errorPtr)
+        public fun fromErrorPointer(errorPtr: CPointer<GError>): Error = Error(errorPtr)
 
-        /**
-         * Creates a new [Error] instance. Unlike `g_error_new()`, [message] isn't a `printf` style format string. Use
-         * this function if [message] contains text you don't have control over, that could include `printf` escape
-         * sequences.
-         * @param domain Error domain.
-         * @param code Error code.
-         * @param message Error message.
-         * @return A new [Error] instance.
-         */
-        public fun fromLiteral(domain: GQuark, code: Int, message: String): Error =
+        public actual fun fromLiteral(domain: UInt, code: Int, message: String): Error =
             Error(g_error_new_literal(domain = domain, code = code, message = message))
 
-        /**
-         * Alters the [error's][Error] literal. Use this function if [message] contains text you don't have control
-         * over. That could include `printf()` escape sequences.
-         * @param domain The error domain.
-         * @param code The error code.
-         * @param message The error message.
-         */
-        public fun changeLiteral(error: Error, domain: GQuark, code: Int, message: String) {
-            g_set_error_literal(err = cValuesOf(error.gErrorPtr), domain = domain, code = code, message = message)
+        public actual fun propagate(dest: Error?, src: Error) {
+            g_propagate_error(if (dest != null) cValuesOf(dest.gErrorPtr) else null, src.gErrorPtr)
         }
+    }
+
+    public actual fun changeLiteral(domain: UInt, code: Int, message: String) {
+        g_set_error_literal(err = cValuesOf(gErrorPtr), domain = domain, code = code, message = message)
     }
 
     override fun close() {
         g_error_free(gErrorPtr)
     }
 
-    /**
-     * Checks to see if the error matches the [domain], and [code]. In particular when error is *null*, *false* will be
-     * returned. If [domain] contains a FAILED (or otherwise generic) error code you should generally not check for it
-     * explicitly, but should instead treat any not explicitly recognized error code as being equivalent to the FAILED
-     * code. This way if the [domain] is extended in the future to provide a more specific error code for a certain
-     * case, your code will still work.
-     * @param domain An error domain.
-     * @param code An error code.
-     * @return If error matches [domain], and [code] then *true*, otherwise *false*.
-     */
-    public fun matches(domain: GQuark, code: Int): Boolean =
+    public actual fun matches(domain: UInt, code: Int): Boolean =
         g_error_matches(error = gErrorPtr, domain = domain, code = code) == TRUE
 }
